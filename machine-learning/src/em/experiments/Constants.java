5f37a55aab98ffe7e26e34c103b0a23cd6d3eec8
Dmitriy Dligach <dimdym@gmail.com>
2013-09-11 13:44:44 -0400

    passive learning curve for em evaluation

---
+++ machine-learning/src/em/experiments/Constants.java
@@ -0,0 +1,15 @@
+package em.experiments;
+
+public class Constants {
+	
+	public static final String dataFile = "/home/dima/active/ibd/data/data.txt";
+	public static final String labelFile = "/home/dima/active/ibd/data/labels-cd.txt";
+	public static final String outputFileRandom = "/home/dima/temp/random.txt";
+	public static final String outputFileActive = "/home/dima/temp/active.txt";
+	
+	public static final int totalFolds = 10; 
+	public static final int seedSize = 0;
+	
+	public static final int rndSeedForSplitting = 0;
+	public static final int rndSeedForSeeding = 0;
+}
