06740ed89eb80ede5598f4db03c2bdd6d1ad8a8d
dimdym <dimdym@gmail.com>
2014-06-27 11:21:19 -0500

    added constant initialization

--- machine-learning/src/em/eval/Evaluate.java
+++ machine-learning/src/em/eval/Evaluate.java
@@ -1,11 +1,16 @@
 package em.eval;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import semsup.eval.Constants;
 
 public class Evaluate {
 
-  public static void main(String[] args) {
-    
+  public static void main(String[] args) throws FileNotFoundException, IOException {
+
+    Constants.populate(Constants.definitions);
+
     for(String phenotype : Constants.phenotypes) {
       EvaluatePhenotype evaluate = new EvaluatePhenotype(phenotype);
       evaluate.start();
