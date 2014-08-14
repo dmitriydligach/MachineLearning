4c617bed2d342923ab1d7d1ffd396735e4900ba4
dimdym <dimdym@gmail.com>
2014-08-14 11:17:22 -0500

    moved feature weight printing to separate method

--- machine-learning/src/em/features/NFoldCvFeatureEval.java
+++ machine-learning/src/em/features/NFoldCvFeatureEval.java
@@ -14,62 +14,70 @@ import com.google.common.base.Function;
 import com.google.common.base.Functions;
 import com.google.common.collect.Ordering;
 
+import data.Alphabet;
 import data.Dataset;
 import data.I2b2Dataset;
 import data.Split;
 import em.implementation.EmModel;
 
 public class NFoldCvFeatureEval {
-	
-	public static void main(String[] args) throws IOException {
+
+  public static void main(String[] args) throws IOException {
 
     if(args.length < 1) {
       System.err.println("Please specify location of the properties file");
     } else {
       Constants.populate(args[0], false);  
     }
-	  
-		I2b2Dataset dataset = new I2b2Dataset();
-		dataset.loadCSVFile(Constants.msData, Constants.msLabels);
-    dataset.mapLabels(Constants.msSourceLabels, Constants.msTargetLabel);
-		dataset.makeAlphabets();
-		
-		Split[] splits = dataset.split(Constants.folds);
-		double cumulAcc = 0;
-		
-		// run just one fold and look at feature weights
-		for(int fold = 0; fold < 1; fold++) {
-			Dataset trainSet = splits[fold].getPoolSet();
-			Dataset testSet = splits[fold].getTestSet();
-			
-			trainSet.makeAlphabets();
-			trainSet.makeVectors();
-			testSet.setAlphabets(trainSet.getLabelAlphabet(), trainSet.getFeatureAlphabet());
-			testSet.makeVectors();
 
-			trainSet.setInstanceClassProbabilityDistribution(new HashSet<String>(dataset.getLabelAlphabet().getStrings()));
-			EmModel classifier = new EmModel(dataset.getLabelAlphabet(), 1.0);
-			classifier.train(trainSet);
-			double accuracy = classifier.test(testSet);
-			cumulAcc = cumulAcc + accuracy;
-			
-			Map<String, Double> featureWeights = classifier.computeFeatureWeights(dataset.getFeatureAlphabet());
-	    List<String> featureNamesSortedByWeight = new ArrayList<String>(featureWeights.keySet());
-	    Function<String, Double> getValue = Functions.forMap(featureWeights);
-	    Collections.sort(featureNamesSortedByWeight, Ordering.natural().reverse().onResultOf(getValue));
-			
-	    CuiLookup mapper = null;
-	    mapper = new CuiLookup("resources/snomed-only-uniq-codes.txt");
-	    for(String feature : featureNamesSortedByWeight) {
-	      String capitalizedNegationRemoved = feature.replace("c", "C").replace("-", "");
-	      String text = mapper.getTerm(capitalizedNegationRemoved);
-	      if(text == null) {
-	        text = "n/a";
-	      }
-	      System.out.format("%12s %-60s %.4f\n", feature, text, featureWeights.get(feature));
-	    }
-		}
-		
+    I2b2Dataset dataset = new I2b2Dataset();
+    dataset.loadCSVFile(Constants.msData, Constants.msLabels);
+    dataset.mapLabels(Constants.msSourceLabels, Constants.msTargetLabel);
+    dataset.makeAlphabets();
+
+    Split[] splits = dataset.split(Constants.folds);
+    double cumulAcc = 0;
+
+    // run just one fold and look at feature weights
+    for(int fold = 0; fold < 1; fold++) {
+      Dataset trainSet = splits[fold].getPoolSet();
+      Dataset testSet = splits[fold].getTestSet();
+
+      trainSet.makeAlphabets();
+      trainSet.makeVectors();
+      testSet.setAlphabets(trainSet.getLabelAlphabet(), trainSet.getFeatureAlphabet());
+      testSet.makeVectors();
+
+      trainSet.setInstanceClassProbabilityDistribution(new HashSet<String>(dataset.getLabelAlphabet().getStrings()));
+      EmModel classifier = new EmModel(dataset.getLabelAlphabet(), 1.0);
+      classifier.train(trainSet);
+      double accuracy = classifier.test(testSet);
+      cumulAcc = cumulAcc + accuracy;
+      
+      displayFeatureWeights(classifier, dataset.getFeatureAlphabet());
+    }
+
     // System.out.format("%d-fold cv accuracy: %.4f\n", Constants.folds, cumulAcc / Constants.folds);
-	}
+  }
+
+  /**
+   * Print feature weights.
+   */
+  public static void displayFeatureWeights(EmModel model, Alphabet featureAlphabet) throws IOException {
+
+    Map<String, Double> featureWeights = model.computeFeatureWeights(featureAlphabet);
+    List<String> featureNamesSortedByWeight = new ArrayList<String>(featureWeights.keySet());
+    Function<String, Double> getValue = Functions.forMap(featureWeights);
+    Collections.sort(featureNamesSortedByWeight, Ordering.natural().reverse().onResultOf(getValue));
+
+    CuiLookup mapper = new CuiLookup("resources/snomed-only-uniq-codes.txt");
+    for(String feature : featureNamesSortedByWeight) {
+      String capitalizedNegationRemoved = feature.replace("c", "C").replace("-", "");
+      String text = mapper.getTerm(capitalizedNegationRemoved);
+      if(text == null) {
+        text = "n/a";
+      }
+      System.out.format("%12s %-60s %.4f\n", feature, text, featureWeights.get(feature));
+    }
+  }
 }
